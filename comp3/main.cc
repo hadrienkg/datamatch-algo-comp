11d4f42148b41ce28cb22b121ba04a3d7d106cb0
hadrienkg <hadrienk0711@gmail.com>
2026-02-02 21:23:42 -0500

    Improved noDormMatch functionality to account for no dorm listed

--- comp3/main.cc
+++ comp3/main.cc
@@ -365,8 +365,9 @@ int main(int argc, char** argv) {
         /// you want.
         logger.log(INFO, "Prevent same-dorm matching");
         for (size_t i = 0; i < tucount; ++i) {
-            for (size_t j = 0; j < tucount; ++j) {
-                if (users[i].no_house_matches == true && users[i].house == users[j].house) {
+            for (size_t j = i; j < tucount; ++j) {
+                if (users[i].no_house_matches == true && users[i].house == users[j].house 
+                    && !users[i].house.empty() ) {
                     weights[i][j] = -1;
                     weights[j][i] = -1;
                     matchtypes[i][j] = -1;
